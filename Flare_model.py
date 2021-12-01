da18ddf7b103a626a8fcc29ff7ebe0eee9375a39
Lupita Tovar <tovarg@uw.edu>
2021-12-01 15:46:24 -0800

    Editing Example 1

--- Flare_model.py
+++ Flare_model.py
@@ -1,7 +1,28 @@
 import numpy as np
 from scipy import special
 from scipy.stats import binned_statistic
-from Flare_Eqn import flare_eqn
+
+def flare_eqn(t,tpeak,fwhm,ampl):
+    '''
+    The equation that defines the shape for the Continuous Flare Model
+    '''
+    #Values were fit & calculated using MCMC 256 walkers and 30000 steps
+
+    A,B,C,D1,D2,f1 = [0.9687734504375167,-0.251299705922117,0.22675974948468916,
+                      0.15551880775110513,1.2150539528490194,0.12695865022878844]
+
+    # We include the corresponding errors for each parameter from the MCMC analysis
+
+    A_err,B_err,C_err,D1_err,D2_err,f1_err = [0.007941622683556804,0.0004073709715788909,0.0006863488251125649,
+                                              0.0013498012884345656,0.00453458098656645,0.001053149344530907 ]
+
+    f2 = 1-f1
+
+    eqn = ((1 / 2) * np.sqrt(np.pi) * A * C * f1 * np.exp(-D1 * t + ((B / C) + (D1 * C / 2)) ** 2)
+                        * special.erfc(((B - t) / C) + (C * D1 / 2))) + ((1 / 2) * np.sqrt(np.pi) * A * C * f2
+                        * np.exp(-D2 * t+ ((B / C) + (D2 * C / 2)) ** 2) * special.erfc(((B - t) / C) + (C * D2 / 2)))
+    return eqn * ampl
+
 
 def flare_model(t,tpeak, fwhm, ampl, upsample=False, uptime=10):
     '''
